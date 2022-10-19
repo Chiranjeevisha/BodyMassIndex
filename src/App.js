e697bfca53b98fae6295985f90251a771b2fcbcd
Chiranjeevisha <90556993+Chiranjeevisha@users.noreply.github.com>
2022-10-19 12:01:50 +0530

    minor changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import BmiCalculator from "./MyLearning-2/bmiCalc";
+
+function App() {
+  return (
+    <div className="App">
+      <BmiCalculator />
+    </div>
+  );
+}
+
+export default App;
